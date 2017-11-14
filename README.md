6089f0dc1ccb08d9bd17a6a5cd7d7abbb4d011c4
overmind <seogarry@gmail.com>
2017-11-15 00:33:01 +0300

    Add auto-pr code
    Add Readme
    
    Add requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # auto-pr
-Automatic PR merge
+
+Automatic PR merge if commit are signed and formatted in special way.
+
+## Configuration
+
+API_KEY = "- PASTE YOUR KEY HERE -"
+ORG_NAME = "organization name" 
+USER_NAME = False # use if your have your own account, instead organization 
+REPO_NAME = "repo-name"
+FILENAME = "filename" # filename to check for signed commits 
+REGEXP = re.compile("\|\s[^@]+@[^@]+\.[^@]+\|")
+
+## Usage 
+
+`python auto-pr.py`
+
---
+++ auto-pr.py
@@ -0,0 +1,47 @@
+from github import Github
+import re
+import sys
+
+API_KEY = "- PASTE YOUR KEY HERE -"
+ORG_NAME = "organization name" 
+USER_NAME = False # use if your have your own account, instead organization 
+REPO_NAME = "repo-name"
+FILENAME = "filename" # filename to check for signed commits 
+REGEXP = re.compile("\|\s[^@]+@[^@]+\.[^@]+\|")
+
+g = Github(login_or_token=API_KEY)
+if USER_NAME:
+    org = g.get_user(USER_NAME)
+else:
+    org = g.get_organization(ORG_NAME)
+    
+r = org.get_repo(REPO_NAME)
+
+is_matched = False
+issues = r.get_pulls('open')
+print("Found {} opened pull requests".format(len(list(issues))))
+for issue in issues:
+    commits = issue.get_commits()
+    for c in commits:
+        files = c.raw_data['files']
+        verified = c.raw_data['commit']['verification']['verified']
+        if not verified:
+            print("Not verified {}".format(c.sha))
+            continue
+        for f in files:
+            if f['filename'] == FILENAME:
+                is_matched = True
+                matched = f['patch'].split('+')
+                if len(matched) > 0:
+                    diff = matched[2]
+                    if REGEXP.match(diff):
+                        try:
+                            issue.merge()
+                            print("Merged. {}".format(c.sha))
+                        except:
+                            print("Unexpected error: {}".format(sys.exc_info()[0]))
+                            # TODO: Errors handling
+                            pass
+                    else:
+                        print("No rules matched for {}".format(c.sha))
+                break;
